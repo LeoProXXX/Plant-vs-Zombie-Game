2041d23022168cedd585051981242e789e4e8278
Sandeep Kumar Singh <44255731+itissandeep98@users.noreply.github.com>
2019-10-21 00:07:15 +0530

    basic login screen update

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,12 +6,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
     public static int width=800;
     public static int height=800;
+    public static  Stage stage;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        stage=primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("LoginScreen.fxml"));
         primaryStage.setTitle("Plant vs Zombies");
         primaryStage.setScene(new Scene(root));
