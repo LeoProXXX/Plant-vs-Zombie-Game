329394d57a5fc686278dba862bbb7532ef88e45c
Sandeep Kumar Singh <44255731+itissandeep98@users.noreply.github.com>
2019-10-23 16:02:03 +0530

    .gitignore updated

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.*;
+
 
 public class Main extends Application {
     public static int width=800;
@@ -19,6 +21,45 @@ public class Main extends Application {
         primaryStage.setTitle("Plant vs Zombies");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+    }
+
+    //A main file that saves the name of all the users that have saved their games
+    public void save_user(){
+
+    }
+
+    public static void serialise(User ob) throws  IOException {
+        ObjectOutputStream out=null;
+        try {
+            out=new ObjectOutputStream(new FileOutputStream(ob.getName()));
+            out.writeObject(ob);
+        }
+        finally{
+            if(out!=null){
+                out.close();
+            }
+
+        }
+    }
+
+    public static User Deserialize(String name) throws IOException,ClassNotFoundException {
+        ObjectInputStream in=null;
+        User player=new User();
+        try{
+            in=new ObjectInputStream(new FileInputStream(name));
+            player = (User) in.readObject();
+        }
+        catch(FileNotFoundException e){
+            serialise(player);
+        }
+        catch (NullPointerException e){
+            serialise(player);
+        }
+        finally{
+            if(in!=null)
+                in.close();
+        }
+        return player;
     }
 
 
